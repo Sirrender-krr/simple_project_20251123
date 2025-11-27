f4e9584f33a6b6d874d8e2dca22a8a09b50228b9
Sirrender-krr <nearamong@gmail.com>
2025-11-27 07:00:39 +0700

    on implementing inventory 3
    now can drag and drop

--- inventory/slot_data.gd
+++ inventory/slot_data.gd
@@ -9,3 +9,11 @@ class_name SlotData
 		quantity = clamp(value,1,item_data.MAX_STACK_SIZE)
 		if quantity > 1 and not item_data.stackable:
 			quantity = 1
+
+func can_fully_merge_with(other_slot_data: SlotData) -> bool:
+	return item_data == other_slot_data.item_data\
+	and item_data.stackable\
+	and quantity + other_slot_data.quantity < item_data.MAX_STACK_SIZE
+
+func fully_merge_with(other_slot_data: SlotData) -> void:
+	quantity += other_slot_data.quantity
--- scripts/inventory_interface.gd
+++ scripts/inventory_interface.gd
@@ -1,10 +1,30 @@
 extends Control
 
+var grabbed_slot_data: SlotData
+
 @onready var player_inventory: PanelContainer = $PlayerInventory
+@onready var grab_slot: PanelContainer = $GrabSlot
+
+func _physics_process(_delta: float) -> void:
+	if grab_slot.visible:
+		grab_slot.global_position = get_global_mouse_position() + Vector2(1,1)
+		grab_slot.scale = Vector2(0.8,0.8)
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
 
 func on_inventory_interact(inventory_data: InventoryData, index: int, button: int) -> void:
-	print("%s %s %s" % [inventory_data, index, button])
+	match [grabbed_slot_data, button]:
+		[null,MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.grab_slot_data(index)
+		[_,MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data,index)
+	update_grabbed_slot()
+
+func update_grabbed_slot() -> void:
+	if grabbed_slot_data:
+		grab_slot.show()
+		grab_slot.set_slot_data(grabbed_slot_data)
+	else:
+		grab_slot.hide()
