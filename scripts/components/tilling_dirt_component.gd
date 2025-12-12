8e7f58e1b6f6f475b8cf943557b77dd2163bcca9
Sirrender-krr <nearamong@gmail.com>
2025-12-12 23:06:12 +0700

    fix tilling and removing position
    - now player can till dirt and remove dirt properly

--- scripts/components/tilling_dirt_component.gd
+++ scripts/components/tilling_dirt_component.gd
@@ -27,56 +27,59 @@ func _unhandled_input(event: InputEvent) -> void:
 #
 #
 #
-#func get_cell_under_mouse() -> void:
-	#mouse_position = ground_tilemap_layer.get_local_mouse_position()
-	#cell_position = ground_tilemap_layer.local_to_map(mouse_position)
-	#cell_source_id = ground_tilemap_layer.get_cell_source_id(cell_position)
-	#local_cell_positon = ground_tilemap_layer.map_to_local(cell_position)
-	#distance = player.global_position.distance_to(local_cell_positon)
-	#print(
-		#"\nmouse_postion: ", mouse_position,
-		#"\ncell_postion: ", cell_position,
-		#"\ncell_source_id: ", cell_source_id,
-		#"\ndistance: ", distance
-	#)
-
-func _ready() -> void:
-	player.tilling.connect(add_tilled_dirt_cell)
-
-func add_tilled_dirt_cell() -> void:
-	direction = player.direction
+func get_cell_under_mouse() -> void:
 	mouse_position = ground_tilemap_layer.get_local_mouse_position()
 	cell_position = ground_tilemap_layer.local_to_map(mouse_position)
 	cell_source_id = ground_tilemap_layer.get_cell_source_id(cell_position)
 	local_cell_positon = ground_tilemap_layer.map_to_local(cell_position)
 	distance = player.global_position.distance_to(local_cell_positon)
 	
+
+func _ready() -> void:
+	player.tilling.connect(add_tilled_dirt_cell)
+
+func add_tilled_dirt_cell() -> void:
+	direction = player.direction
+	get_cell_under_mouse()
+	
 	var player_grid = ground_tilemap_layer.local_to_map(player.global_position)
 	var tilled_pos:Array = []
-	match direction:
-		dir.left:
-			tilled_pos = [Vector2i(player_grid.x-1,cell_position.y)]
-		dir.right:
-			tilled_pos = [Vector2i(player_grid.x+1,cell_position.y)]
-		dir.up:
-			tilled_pos = [Vector2i(cell_position.x, player_grid.y-1)]
-		dir.down:
-			tilled_pos = [Vector2i(cell_position.x, player_grid.y+1)]
+	tilled_pos = tilling_dir(player_grid,cell_position)
 	
 	if distance <= player_radius and player.tools == 1 and cell_source_id != -1:
 		tilled_dirt_tilemap_layer.set_cells_terrain_connect(tilled_pos,tilled_dirt_terrain_set,tilled_dirt_terrain)
 
 func remove_tilled_dirt_cell() -> void:
+	direction = player.direction
+	get_cell_under_mouse()
 	var player_grid = ground_tilemap_layer.local_to_map(player.global_position)
 	var tilled_pos:Array = []
+	tilled_pos = tilling_dir(player_grid,cell_position)
+	if distance <= player_radius and player.tools == 1 and cell_source_id != -1:
+		print('removing')
+		tilled_dirt_tilemap_layer.set_cells_terrain_connect(tilled_pos,0,-1)
+
+func tilling_dir(player_grid, cell_position) -> Array:
+	var tilled_pos: Array
 	match direction:
 		dir.left:
-			tilled_pos = [Vector2i(player_grid.x-1,cell_position.y)]
+			if cell_position.x >player_grid.x:
+				tilled_pos = [Vector2i(player_grid.x,cell_position.y)]
+			else:
+				tilled_pos = [cell_position]
 		dir.right:
-			tilled_pos = [Vector2i(player_grid.x+1,cell_position.y)]
+			if cell_position.x < player_grid.x:
+				tilled_pos = [Vector2i(player_grid.x,cell_position.y)]
+			else:
+				tilled_pos = [cell_position]
 		dir.up:
-			tilled_pos = [Vector2i(cell_position.x, player_grid.y-1)]
+			if cell_position.y >player_grid.y:
+				tilled_pos = [Vector2i(cell_position.x,player_grid.y)]
+			else:
+				tilled_pos = [cell_position]
 		dir.down:
-			tilled_pos = [Vector2i(cell_position.x, player_grid.y+1)]
-	if distance <= player_radius and player.tools == 1 and cell_source_id != -1:
-		tilled_dirt_tilemap_layer.set_cells_terrain_connect(tilled_pos,0,-1)
+			if cell_position.y <player_grid.y:
+				tilled_pos = [Vector2i(cell_position.x,player_grid.y)]
+			else:
+				tilled_pos = [cell_position]
+	return tilled_pos
