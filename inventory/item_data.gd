a68a09be6debac8bbc95a219ebfa1584fa660112
Sirrender-krr <nearamong@gmail.com>
2026-01-10 11:11:19 +0700

    on implementing shop

--- inventory/item_data.gd
+++ inventory/item_data.gd
@@ -5,6 +5,8 @@ class_name ItemData
 @export_multiline var description: String = ""
 @export var stackable: bool = false
 @export var MAX_STACK_SIZE: int = 1
+@export var buy: int
+@export var sell: int
 @export var texture: AtlasTexture
 
 func use(target) -> void:
